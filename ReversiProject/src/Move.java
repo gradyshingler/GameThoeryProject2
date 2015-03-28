27745c057ae8c791ee082072ccd01435b52d2fec
ColdMillenium <andrewgerribrowning@gmail.com>
2015-03-28 14:48:48 -0400

    Finished Execute for Move

--- ReversiProject/src/Move.java
+++ ReversiProject/src/Move.java
@@ -4,8 +4,8 @@ import java.util.List;
 import java.util.TreeMap;
 
 public class Move {
-	int xPos;
-	int yPos;
+	int row;
+	int col;
 	int PrevScore;
 	int CurrScore;
 	int player;// 1 is player1, 2 is player2
@@ -14,9 +14,9 @@ public class Move {
 		N, NE, E, SE, S, SW, W, NW
 	};
 	public TreeMap<Direction, Integer> Flips;
-	public Move(int p, int x1, int y1) {
-		xPos = x1;
-		yPos = y1;
+	public Move(int p, int r, int c) {
+		row = r;
+		col= c;
 		player = p;
 		Flips = new TreeMap<Direction, Integer>();
 	}
@@ -24,8 +24,8 @@ public class Move {
 	public void addFlips(Direction dir, int num){
 		Flips.put(dir, num);
 	}
-	public void addBoardChange(int x, int y, int sc1, int sc2){
-		BoardChanges.add("x,y,sc1,sc2");
+	public void addBoardChange(int r, int c, int sc1, int sc2){
+		BoardChanges.add(r + "," + c + "," + sc1 + "," + sc2);
 	}
 	public void Execute(){
 		for (Direction dir : Flips.keySet()){
@@ -33,49 +33,50 @@ public class Move {
 			{
 				for(int i = 0 ; i< Flips.get(dir); i++)
 				{
-					//Board.board[x][y-1] need to flip in opposite direction of x and y destination coordinates
+					Board.board[row-i][col].flip();
+					//Board.board[row][col-i] need to flip in opposite direction of x and y destination coordinates
 				}
 			}else if(dir.equals(Direction.NE))
 			{
 				for(int i = 0 ; i< Flips.get(dir); i++)
 				{
-					
+					Board.board[row-i][col-i].flip();
 				}
 			}else if(dir.equals(Direction.E))
 			{
 				for(int i = 0 ; i< Flips.get(dir); i++)
 				{
-					
+					Board.board[row][col+i].flip();
 				}
 			}else if(dir.equals(Direction.SE))
 			{
 				for(int i = 0 ; i< Flips.get(dir); i++)
 				{
-					
+					Board.board[row+i][col-i].flip();
 				}
 			}else if(dir.equals(Direction.S))
 			{
 				for(int i = 0 ; i< Flips.get(dir); i++)
 				{
-					
+					Board.board[row+i][col].flip();
 				}
 			}else if(dir.equals(Direction.SW))
 			{
 				for(int i = 0 ; i< Flips.get(dir); i++)
 				{
-					
+					Board.board[row+i][col+i].flip();
 				}
 			}else  if(dir.equals(Direction.W))
 			{
 				for(int i = 0 ; i< Flips.get(dir); i++)
 				{
-					
+					Board.board[row][col+i].flip();
 				}
 			}else if(dir.equals(Direction.NW))
 			{
 				for(int i = 0 ; i< Flips.get(dir); i++)
 				{
-					
+					Board.board[row-i][col+i].flip();
 				}
 			}else{
 				System.out.printf("Direction Error in move.execute");
@@ -86,15 +87,15 @@ public class Move {
 		
 	}
 	public int getConvertedRow() {
-		return xPos;
+		return col;
 
 	}
 
 	public int getConvertedCol() {
-		if (xPos <= 4)
-			return yPos + (xPos - 4);
+		if (col <= 4)
+			return row + (col - 4);
 		else
-			return yPos - ((xPos - 5));
+			return row - ((col - 5));
 	}
 
 	//This should be used to print the one and final move to submit
@@ -105,7 +106,7 @@ public class Move {
 	public boolean equals(Object o) {
 		if (o instanceof Move) {
 			Move tempMove = (Move) o;
-			return (xPos == tempMove.xPos && yPos == tempMove.yPos);
+			return (col == tempMove.col && row == tempMove.row);
 		} else {
 			return false;
 		}
@@ -113,6 +114,6 @@ public class Move {
 	}
 
 	public String toString() {
-		return xPos + ":" + yPos;
+		return col + ":" + row;
 	}
 }
