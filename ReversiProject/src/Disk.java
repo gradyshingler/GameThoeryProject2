336e76b7f791a8b1ccb9e5e315c21417f5de2755
javerbukh <javerbukh@10.108.94.93>
2015-03-28 16:01:03 -0400

    Equals method in Disk

--- ReversiProject/src/Disk.java
+++ ReversiProject/src/Disk.java
@@ -42,4 +42,11 @@ public class Disk {
 		if(cell == Cell.MINE) cell = Cell.OPPONENT;
 		else if(cell == Cell.OPPONENT) cell = Cell.MINE;
 	}
+	public void equals(Disk o){
+		if(this.getxPos() == o.getxPos() && this.getyPos() == o.getyPos() &&
+				this.getCell() == o.getCell()){
+			return true;
+		}
+		return false;
+	}
 }
